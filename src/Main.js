5f214c95ba111ce7864252d47fafe44efa473fc0
Krzysztof <krzysztof.swiatek@student.uj.edu.pl>
2023-06-11 06:14:45 +0200

    some fixes

--- src/Main.js
+++ src/Main.js
@@ -6,15 +6,12 @@ import { Link  } from 'react-router-dom';
 function Main() {
 
     const [images, setImages] = useState([]);
-    const [isLoaded, setIsLoadedd ] = useState(false);
     const [lastIndex, setLastIndex] = useState(0);
     const [hasMore, setHasMore] = useState(true);
-    //const [isScrolled, setIsScrolled] = useState(false);
     const maxItems = 3;
 
     const updateHash = ((event) => {
-        //setIsScrolled(false);
-        //window.location.hash = '#' + window.scrollY;
+        window.location.hash = '#' + event.target.id;
     });
 
     const loadData = (() => {
@@ -23,39 +20,36 @@ function Main() {
             const data = store.getAll(query, maxItems);
             data.onsuccess = (event) => {
                 let new_images = []
+                let last = 0;
                 data.result.forEach(d => {
                         const blob = d.image.file;
                         const url = URL.createObjectURL(blob);
                         new_images.push({id: d.id, image: url, comments: d.comments});
-                        setLastIndex(d.id);
+                        last = d.id;
                     }
                 );
                 if (new_images.length === 0) {
                     setHasMore(false);
                     return;
                 }
+                setLastIndex(last);
                 setImages([...images, ...new_images]);
             };
         }); 
     });
 
     useEffect(() => {
-        /*if (!isScrolled) {
-            const destY = parseInt(window.location.hash.substring(1))
-            if (isNaN(destY)) {
-                setIsScrolled(true);
-                return;
+        if (window.location.hash && window.location.hash.length > 0) {
+            //this works, even though i dont think it should
+            let targetElement = document.getElementById(window.location.hash.substring(1));
+            if (!targetElement) {
+                targetElement = document.getElementById(lastIndex);
             }
-            window.scrollTo(window.scrollX, destY);
-            if (window.scrollY === destY) {
-                setIsScrolled(true);
+            if (targetElement) {
+                targetElement.scrollIntoView({behavior: 'smooth'});
             }
-        }*/
-        if (isLoaded) {
-            return;
         }
         loadData();
-        setIsLoadedd(true);
     });
 
     return (
