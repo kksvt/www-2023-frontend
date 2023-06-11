c7f54a96b65dc208d1bc2d7adb40c95323143628
Krzysztof <krzysztof.swiatek@student.uj.edu.pl>
2023-06-11 15:33:17 +0200

    fixed an error in the faq

--- src/Faq.js
+++ src/Faq.js
@@ -6,7 +6,7 @@ class Faq extends Component {
         return (
             <div>
                 <h1>Frequently Asked Questions</h1>
-                <Accordion title='What is this website?' description="It's a simple imageboard frontend written in React." />
+                <Accordion title='What is this website?' description="This website is a simple imageboard frontend written in React." />
                 <Accordion title='How are the images stored?' description="The images are stored in an IndexedDB called 'imageboard_db' in the 'files' store. "/>
                 <Accordion title="Can I leave comments under the images?" description="Certainly! In order to add a comment to an image, click on the image on the Main Page, which should redirect you to the '/comments/:imageId' page. There you can leave a comment and view previous comments."/>
                 <Accordion title="What has assisted you in creating this website?" description="I've used sample code from mozilla.org, stackoverflow.com, css-tricks.com, lab homeworks, among many other resources."/>
